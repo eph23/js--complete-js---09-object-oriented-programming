8ad5f76eecf22ceb03c89357703f865919a800bc
ephraim <ephraims.818@gmail.com>
2025-08-07 02:07:53 +0600

    JS--COMPLETE-JS---OBJECT-ORIENTED-PROGRAMMING - 09. Inheritance Between Classes ES6 Classes

---
+++ 09. Inheritance Between Classes ES6 Classes/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// Class
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name`);
+    }
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static
+  static hey() {
+    console.log(`Hey ${this.fullName}`);
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName}, and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I am ${
+        2037 - this.birthYear
+      } years old, but as a student  I fell more like ${
+        2037 - this.birthYear + 10
+      } years old`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+console.log(martha);
+martha.introduce();
+martha.calcAge();
